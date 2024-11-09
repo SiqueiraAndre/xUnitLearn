7ef2f06e03db5679a57b727d57a3ac5c81038047
André Siqueira <siqueira.andre@gmail.com>
2024-11-09 11:29:47 -0300

    Corrigindo problema de mutação

--- 3639-csharptestes-jornadamilhas-curso1/tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
+++ 3639-csharptestes-jornadamilhas-curso1/tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -22,15 +22,15 @@ public class OfertaViagemDesconto
         Assert.Equal(precoComDesconto, oferta.Preco);
     }
     
-    [Fact]
-    public void RetornaDescontoMaximoQuandoValorDescontoMaiorPreco()
+    [Theory]
+    [InlineData(120, 30)]
+    [InlineData(100, 30)]
+    public void RetornaDescontoMaximoQuandoValorDescontoMaiorOuIgualPreco( double desconto, double precoComDesconto)
     {
         //arrange
         Rota rota = new Rota("OrigemA", "OrigemB");
         Periodo periodo = new Periodo(new DateTime(2024, 06, 01), new DateTime(2024, 06, 10));
         double precoOriginal = 100.00;
-        double desconto = 120.00;
-        double precoComDesconto = 30.00;
         OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
         
         //act
